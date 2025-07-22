adbcb6635f48765609e1702e454c7e73787a54e0
devamgangar <devamgangar@gmail.com>
2025-07-23 01:57:51 +0530

    Create pairs.cpp

---
+++ pairs.cpp
@@ -0,0 +1,28 @@
+//1)PAIRS
+ //pairs!!  u can store 2 elements with differnet data types
+
+    pair < int, float > p = {
+        1,
+        3.69
+    };
+    cout << p.first; //first dega
+    cout << p.second; //second dega
+
+    //pair array
+    pair < int, int > arr[3] = {
+        {
+            1,
+            2
+        },
+        {
+            2,
+            3
+        },
+        {
+            3,
+            4
+        }
+    };
+    cout<<arr[2].second;
+
+
